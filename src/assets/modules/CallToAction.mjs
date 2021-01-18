ec92ca93d4ecef91c0e297c5c6ab57e95b3a2c22
jascha <jascha@jaeh.at>
2021-01-18 04:10:59 +0100

    rename HeaderImage to CallToAction

---
+++ src/assets/modules/CallToAction.mjs
@@ -0,0 +1,13 @@
+export const View = ({ src }) => img({
+  class: 'CallToAction',
+  src,
+  width: '1200',
+  height: '900',
+})
+
+export const style = {
+  float: 'right',
+  position: 'relative',
+  clear: 'both',
+  margin: '0 0 60px',
+}
