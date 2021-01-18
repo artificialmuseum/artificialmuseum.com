bc71f580165115d8f3b66104fd52a9d6904c2edc
jascha <jascha@jaeh.at>
2021-01-18 03:44:26 +0100

    modules/Slogan: add slogan in props, update styles

--- src/assets/modules/Slogan.mjs
+++ src/assets/modules/Slogan.mjs
@@ -1,16 +1,15 @@
-export const View = () =>
-  div({ class: 'Slogan' }, [
-    p("WE DON'T NEED SPACE,"),
-    p('TIME IS IRRELEVANT,'),
-    p('OBJECTS STAY AS USEFUL'),
-    p('AS THE USER.'),
-  ])
+export const View = children =>
+  div(
+    { class: 'Slogan' },
+    children.map(c => p(c)),
+  )
 
 export const style = v => ({
-  fontSize: '40px',
+  clear: 'both',
+  fontSize: '1.5em',
   fontWeight: 600,
   lineHeight: 1,
+  margin: '2em 0',
   padding: `0 ${v.paddingLeft}`,
   width: '100%',
-  margin: '2em 0',
 })
