756ea365f296c2eaf7631a2db13a1920bf114812
jascha <jascha@jaeh.at>
2021-01-18 04:11:36 +0100

    Footer: remove divs

--- src/assets/modules/Footer.mjs
+++ src/assets/modules/Footer.mjs
@@ -1,50 +1,46 @@
 export const View = state =>
   footer({ class: 'Footer' }, [
     div({ class: 'Container' }, [
-      div([
-        svg({ class: 'logo', viewBox: '0 0 54 60' }, [
-          path({ d: 'M36 0v10h9v40h-9v10h18V0zM0 0v60h18V50H9V10h9V0z', fill: '#ffffff' }),
-        ]),
-
-        div([
-          h2('Impressum'),
-
-          h3('Address:'),
-          p('TheSystemCollective, z.H. Daniela Weiss'),
-          p('Yppenplatz 5'),
-          p('1170 Wien'),
-          p('Austria'),
-        ]),
-
-        h3('Represented by:'),
-        p('Litto / Daniela Weiss'),
-
-        h3('Email:'),
-        Link({
-          to: 'mailto:thesystemcollective@gmail.com',
-          text: 'thesystemcollective@gmail.com',
-        }),
+      svg({ class: 'logo', viewBox: '0 0 54 60' }, [
+        path({ d: 'M36 0v10h9v40h-9v10h18V0zM0 0v60h18V50H9V10h9V0z', fill: '#ffffff' }),
       ]),
 
       div([
-        h2('Privacy notice'),
+        h2('Impressum'),
 
-        h3('No spying'),
-        p('We promise to never analyze, track, or sell any data about you.'),
-        p('We will also never save any data without asking explicitly.'),
+        h3('Address:'),
+        p('TheSystemCollective, z.H. Daniela Weiss'),
+        p('Yppenplatz 5'),
+        p('1170 Wien'),
+        p('Austria'),
+      ]),
 
-        h3('Local data'),
-        p([
-          'All browser features that collect sensitive data',
-          ' (device orientation, 3d-map of your surroundings, camera video streams, geolocation)',
-          ' DO NOT and NEVER WILL transmit this data to our servers.',
-        ]),
+      h3('Represented by:'),
+      p('Litto / Daniela Weiss'),
 
-        h3('Session storage'),
-        p([
-          'We save some data in your browser (your last gps location).',
-          'This information gets deleted as soon as you close the browser tab.',
-        ]),
+      h3('Email:'),
+      Link({
+        to: 'mailto:thesystemcollective@gmail.com',
+        text: 'thesystemcollective@gmail.com',
+      }),
+
+      h2('Privacy notice'),
+
+      h3('No spying'),
+      p('We promise to never analyze, track, or sell any data about you.'),
+      p('We will also never save any data without asking explicitly.'),
+
+      h3('Local data'),
+      p([
+        'All browser features that collect sensitive data',
+        ' (device orientation, 3d-map of your surroundings, camera video streams, geolocation)',
+        ' DO NOT and NEVER WILL transmit this data to our servers.',
+      ]),
+
+      h3('Session storage'),
+      p([
+        'We save some data in your browser (your last gps location).',
+        'This information gets deleted as soon as you close the browser tab.',
       ]),
 
       Social(state),
