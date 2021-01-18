dbc8a2225105fec32caaa5d56d4526aa5ef435e1
jascha <jascha@jaeh.at>
2021-01-18 03:44:48 +0100

    theme: add vars.paddingLeft, update styles

--- src/assets/themes/artificialmuseum/index.mjs
+++ src/assets/themes/artificialmuseum/index.mjs
@@ -16,8 +16,8 @@ export const vars = {
     light: '#0f0f0f',
 
     hover: {
-      dark: '#f0f0f0',
-      light: '#0f0f0f',
+      dark: '#ffffff',
+      light: '#000000',
     },
   },
 
@@ -27,6 +27,8 @@ export const vars = {
     desktop: '940px',
     agency: '1200px',
   },
+
+  paddingLeft: '25px',
 }
 
 export default (v = {}) => {
@@ -36,49 +38,51 @@ export default (v = {}) => {
     '@font-face': {
       family: 'notosans',
       url: 'https://static.thesystem.at/font/',
-      weights: [400, 600],
       styles: ['normal', 'italic'],
       types: ['woff', 'woff2'],
+      weights: [400, 600],
       unicodeRange:
         'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
     },
 
     body: {
-      fontSize: '18px',
       fontFamily: 'notosans, sans-serif',
-      overflowX: 'initial',
+      fontSize: '20px',
       lineHeight: 1.4,
+      overflowX: 'initial',
+    },
+
+    a: {
+      color: v.link.light,
+
+      '&:hover': {
+        color: v.link.hover.light,
+      },
+    },
+
+    h2: {
+      fontSize: '1.5em',
     },
 
     '.Wrapper': {
-      width: '100%',
       maxWidth: 'inherit',
+      width: '100%',
     },
 
     img: {
       height: 'auto',
-      width: '100%',
+      maxHeight: '100%',
+      maxWidth: '100%',
     },
 
-    '.header': {
-      'svg.logo': {
-        height: '96px',
-        float: 'left',
-        margin: '20px 20px 20px 0',
-      },
+    '.text': {
+      clear: 'both',
+      margin: '2em 0',
+      maxWidth: '1000px',
+      padding: `0 ${v.paddingLeft}`,
 
-      h2: {
-        fontSize: '2.7em',
-        margin: 0,
-        padding: 0,
-        lineHeight: '1.2',
-        padding: '9px 0 0',
-        fontWeight: 600,
-      },
-
-      h3: {
-        fontSize: '1.5em',
-        margin: '31px 0 0',
+      p: {
+        margin: '0 0 1em',
       },
     },
 
